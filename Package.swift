c75a298425af10f580012a26d1ea5a2437b38407
pjechris <jcpastant@xebia.fr>
2019-02-03 17:15:09 +0100

    [spm] Add Sourcery

--- Package.swift
+++ Package.swift
@@ -12,12 +12,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Swinject/Swinject.git", from: "2.4.0"),
+        .package(url: "https://github.com/krzysztofzablocki/Sourcery", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AnnotationInject",
-            dependencies: [])
+            dependencies: ["Swinject", "SourceryRuntime"],
+            path: "Sources")
     ]
 )
