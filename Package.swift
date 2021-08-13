38866fae5b94254a7bab17697449dc2baed57047
pjechris <pjechris@users.noreply.github.com>
2021-08-13 21:32:54 +0200

    [spm] added resources

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -25,7 +25,11 @@ let package = Package(
         .target(
             name: "AnnotationInject",
             dependencies: ["Swinject", "SourceryRuntime"],
-            path: "Sources"),
+            path: "Sources",
+            resources: [
+                .copy("Templates"),
+                .copy("Scripts")
+            ]),
         .testTarget(
             name: "AnnotationInjectTests",
             dependencies: ["AnnotationInject", "Quick", "Nimble"],
